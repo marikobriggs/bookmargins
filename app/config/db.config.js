20b6b2b731207698792c7e81981c44c4c2ea3832
MarikoBriggs <marikobriggs@gmail.com>
2021-03-29 22:16:01 -0700

    feat(api): add skeleton code for api routing and database connection

---
+++ app/config/db.config.js
@@ -0,0 +1,9 @@
+/**
+ * @file Configurations for database connection. 
+ * @author Mariko
+ */
+
+export const HOST = process.env.DB_HOST;
+export const USER = process.env.DB_USER;
+export const PASSWORD = process.env.DB_PASSWORD;
+export const DB = process.env.DB_DATABASE;
