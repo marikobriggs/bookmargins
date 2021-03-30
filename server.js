84a0482da64e48a7e85c185f62f62dd9ca5551b6
MarikoBriggs <marikobriggs@gmail.com>
2021-03-29 19:21:04 -0700

    feat(server): add basic webserver code

---
+++ server.js
@@ -0,0 +1,27 @@
+/**
+ * following this tutorial: https://bezkoder.com/node-js-rest-api-express-mysql/
+ */
+
+// importing express + json, no need for bodyParser 
+import pkg from 'express';
+const { express, json } = pkg;
+
+const app = pkg()
+const PORT = 3000
+
+// parse requests of content-type: application/json
+app.use(pkg.json())
+
+// parse requests of content-type: application/x-www-form-urlencoded 
+app.use(pkg.urlencoded({ extended: true }))
+
+// simple route 
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bookmargins" })
+})
+
+// set port + listen for requests 
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT + ".")
+}) 
+
