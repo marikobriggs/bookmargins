97c015e2767f0e90e24b93f59f91481acc4f76d8
MarikoBriggs <marikobriggs@gmail.com>
2021-04-19 14:07:25 -0700

    add blank classes for database tables

---
+++ app/models/bookclub.model.js
@@ -0,0 +1,6 @@
+/**
+ * @file Constructor and CRUD functions for "bookclub" object  
+ * @author Mariko 
+ * following this tutorial: https://bezkoder.com/node-js-rest-api-express-mysql/
+ */
+
---
+++ app/models/readingdetails.model.js
@@ -0,0 +1,6 @@
+/**
+ * @file Constructor and CRUD functions for "reading details" object  
+ * @author Mariko 
+ * following this tutorial: https://bezkoder.com/node-js-rest-api-express-mysql/
+ */
+
